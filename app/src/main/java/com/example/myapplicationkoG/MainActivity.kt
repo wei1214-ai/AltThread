46f8b676d02582becdb23ef7bcb1032b206dd91a
Tzu Heng <yeapth-pm25@student.tarc.edu.my>
2026-08-08 16:36:47 +0800

    change txt

--- app/src/main/java/com/example/myapplicationkoG/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationkoG/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!  fivfjnvidsjnv",
+        text = "Hello $name!  fivfjnvidsjnvmji1111111",
         modifier = modifier
     )
 }
