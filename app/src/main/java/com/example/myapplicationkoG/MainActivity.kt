749efee2f071192d578a370800cb1311e96d1c3a
Ho Yong Ze <hoys-pm25@studemt.tarc.edu.my>
2026-08-08 15:53:44 +0800

    hi

--- app/src/main/java/com/example/myapplicationkoG/MainActivity.kt
+++ app/src/main/java/com/example/myapplicationkoG/MainActivity.kt
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name! yz",
+        text = "Hello $name! fuyuan is gay",
         modifier = modifier
     )
 }
